24f4ad90197b50d4a63c3982d9cb6827de07b960
codertyler <codertylerasai@gmail.com>
2020-10-07 20:37:01 +0000

    dynamically creating tweets using db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,43 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(() => {
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+$(document).ready( () => {
+  
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for(let tweet of tweets) {
+      // calls createTweetElement for each tweet
+      let myTweet = createTweetElement(tweet);
+      $('.tweet').append(myTweet); 
+    
+    }
+    
+    // takes return value and appends it to the tweets container
+
+  }
 
   const createTweetElement = (obj) => {
-    return `<article> 
-     <header>
-              <label for="img"><img src="${obj.user.avatars}" alt="avatar">
-                Newton</label>
-               <h3 id="at_name">${obj.user.handle}</h3>
-            </header>
-            <h4>
-            ${obj.content.text}
-            </h4>
-            
-            <hr>
-            <footer>
-              <p>${Date(obj.created_at).toString()}</p>
-              <p>logos</p>
-            </footer>
-          </article>
-     </article>`;
-  };
+    let $tweet = `<article> 
+                <header>
+               <label for="img"><img src="${obj.user.avatars}" alt="avatar">
+                 Newton</label>
+                <h3 id="at_name">${obj.user.handle}</h3>
+             </header>
+             <h4>
+             ${obj.content.text}
+             </h4>
+             
+             <hr>
+             <footer>
+               <p>${Date(obj.created_at).toString()}</p>
+               <p>logos</p>
+             </footer>
+           </article>
+      </article><br>`;
+      return $tweet;
+      
+    };
 
-  const $tweet = createTweetElement(tweetData);
+    
+    renderTweets(data);
 
-  console.log($tweet);
-
-  $(".tweet").append($tweet);
 });
